8c2beabb248a37243564ada09db6ed301d8c8eac
Roman <yashenkovrv@gmail.com>
2022-09-18 16:27:22 +0300

    add Les2 with diagram

---
+++ HomeWork001/Les2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два числа: ");
+int a, b;
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a + " > " + b);
+}
+else if (b > a)
+{
+    Console.WriteLine(b + " > " + a);
+}
+else
+{
+    Console.WriteLine(b + " = " + a);
+}
