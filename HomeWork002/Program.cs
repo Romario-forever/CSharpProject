3bb1da95642dfa086262df1bfc797d893d0e5720
Roman <yashenkovrv@gmail.com>
2022-09-26 20:43:29 +0300

    Решил домашнее задание к семинару 2

--- HomeWork002/Program.cs
+++ HomeWork002/Program.cs
@@ -6,7 +6,7 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("Задача 10");
             // Напишите программу, которая принимает на вход трёхзначное число 
             // и на выходе показывает вторую цифру этого числа.
@@ -14,7 +14,7 @@ namespace HomeWork
             Console.WriteLine("Введите трехзначное число: ");
             int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
             string stringNumber = Convert.ToString(threeDigitNumber);
-            Console.WriteLine("Вторая цифра этого цисла: " + stringNumber[1]);*/
+            Console.WriteLine("Вторая цифра этого цисла: " + stringNumber[1]);
 
 
             Console.WriteLine();
@@ -22,7 +22,7 @@ namespace HomeWork
             //Напишите программу, которая выводит третью цифру заданного числа 
             //или сообщает, что третьей цифры нет.
 
-            Console.Write("Введи число: ");
+            Console.Write("Введите число: ");
             int anyNum = Convert.ToInt32(Console.ReadLine());
             string anyNumText = Convert.ToString(anyNum);
             if (anyNumText.Length > 2)
@@ -33,6 +33,52 @@ namespace HomeWork
             {
                 Console.WriteLine("Третьей цифры нет");
             }
+
+            Console.WriteLine();
+            Console.WriteLine("Задача 15");
+            // Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+            // и проверяет, является ли этот день выходным.
+            Console.Write("Введите цифру, обозначающую день недели: ");
+            int numberDayWeek = Convert.ToInt32(Console.ReadLine());
+
+            void  CheckingTheDayOfTheWeek (int numberDayWeek) 
+            {
+                if (numberDayWeek == 6 || numberDayWeek == 7)
+                Console.WriteLine("Это выходной день");
+                
+                else if (numberDayWeek < 1 || numberDayWeek > 7)
+                Console.WriteLine("Это НЕ ДЕНЬ недели!");
+                
+                else  Console.WriteLine("Это НЕ выходной день");
+            }
+                CheckingTheDayOfTheWeek(numberDayWeek);
+
+            Console.WriteLine();
+            Console.WriteLine("Задача 228");
+            // Напишите программу, которая принимает на вход семь чисел, 
+            // и находит их среднее арифметическое
+
+            int array;
+            int x;
+            int sum = 0;
+            int rezult;
+
+            Console.WriteLine("Введите количество чисел");
+            array = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < array; i++)
+            {
+
+                Console.WriteLine("Введите заданное количество чисел");
+                x = Convert.ToInt32(Console.ReadLine());
+                sum += x;
+            }
+
+            rezult = sum / array;
+
+            Console.WriteLine(rezult);
+
         }
     }
 }
