0417bfb1247b3311f5bd3adea6536f629df37e7c
Roman <yashenkovrv@gmail.com>
2022-09-28 15:23:17 +0300

    Решил задачу 19

--- HomeWork003/Program.cs
+++ HomeWork003/Program.cs
@@ -1,8 +1,47 @@
-﻿static void Main(string[] args)
-{
-    Console.WriteLine();
-    Console.WriteLine("Задача 19");
-    //Напишите метод, который принимает на вход пятизначное число
-    //и проверяет, является ли оно палиндромом.
+﻿using System;
 
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Задача 19");
+            //Напишите метод, который принимает на вход пятизначное число
+            //и проверяет, является ли оно палиндромом.
+
+            int number = 15951;
+            int current_number = number;
+            int result = 0;
+            
+            if (number > 9999 && number < 100000)
+            {
+                Console.WriteLine("Это пятизначное число");
+            }
+            else
+            {
+                Console.WriteLine("Это не пятизначное число");
+            }
+            while (current_number > 0)
+                {   
+                int digit = current_number % 10;
+                result = result * 10 + digit;
+                current_number /= 10;
+                Console.WriteLine(result);
+                }
+            if (number == result)
+            {
+                Console.WriteLine("Число палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Число не палиндром");
+            }
+            
+
+
+
+        }
+    }
 }
