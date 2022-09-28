b4f0b44f5f1f5e53bd593cf12a8a27cfc8f644bb
Roman <yashenkovrv@gmail.com>
2022-09-28 15:44:08 +0300

    add HomeWork003

--- HomeWork003/Program.cs
+++ HomeWork003/Program.cs
@@ -6,7 +6,7 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            /*Console.WriteLine();
             Console.WriteLine("Задача 19");
             //Напишите метод, который принимает на вход пятизначное число
             //и проверяет, является ли оно палиндромом.
@@ -37,8 +37,23 @@ namespace HomeWork
             else
             {
                 Console.WriteLine("Число не палиндром");
-            }
+            }*/
+
+            Console.WriteLine();
+            Console.WriteLine("Задача 21");
+            // Напишите метод, который принимает на вход координаты двух точек
+            // и находит расстояние между ними в 3D пространстве.
+
+            int a_x = 5, a_y = 2, a_z = 3;
+            int b_x = 1, b_y = 2, b_z = 5;              
             
+
+            double result = Math.Sqrt(Math.Pow(a_x - b_x, 2)
+                                + Math.Pow(a_y - b_y, 2)
+                                + Math.Pow(a_z - b_z, 2));
+
+            Console.WriteLine(result);
+
 
 
 
