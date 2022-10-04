e3817b91e725e4edad89c59a680aa492096f494d
Roman <yashenkovrv@gmail.com>
2022-10-04 15:44:59 +0300

    Выполнена работа к семинару 5

--- HomeWork005/Program.cs
+++ HomeWork005/Program.cs
@@ -7,48 +7,151 @@ namespace HomeWork
 
         public static void Main(string[] args)
         {
-
+            Console.WriteLine();
             Console.WriteLine("Задача 34");
             Console.WriteLine();
             //Задайте массив заполненный случайными положительными трёхзначными числами. 
             //Напишите программу, которая покажет количество чётных чисел в массиве.
-            //[345, 897, 568, 234] -> 2
             
-            int[] plusNumbers = new int[4];
-            
-            int[] numbers = plusNumbers;
-            int length = numbers.Length;
-            int index = 0;
-                
-                while (index < length)
-                {
-                    numbers[index] = new Random().Next(99, 1000);
-                    index++;
-                    Console.WriteLine(numbers(index));
-                }
-            
-            
-            /*void PrintArray(int[] values)
+            int[] GenerateRandomArray(int size, int start, int end)
             {
-                int count = values.Length;
-                int position = 0;
-                while (position < count)
+                //объявляем массив размера size( количество элементов = size)
+                // это выделена область памяти для переменных типа int 
+                //(количество переменных = size)
+                int[] RandomArray = new int[size];
+                for (int i = 0; i < size; i++)
                 {
-                    Console.WriteLine(values(position));
-                    position++;
+                //Генерируем число из диапазона [start;end] и записываем его в i-йэлемент массива
+                    RandomArray[i] = new Random().Next(start, end + 1);
                 }
-            }*/
+                
+                return RandomArray;
+            }
+
+            //Метод для вывода элементов массива в консоль для вывода на экрана
+            //передаём "()" в метод ShowArray массив "int[] "
+            void ShowArray(int[] Array1)
+            {
+                for (int i = 0; i < Array1.Length; i++)
+                {
+                    Console.WriteLine(Convert.ToString(Array1[i]));
+                }
+            }
+
+            // Проверка количества чётных чисел (элементов) массива
+            int CountOfEven(int[] myArray2)
+            //передаём "()" в метод ShowArray массив "int[] "
+            {
+                
+                int Count = 0;
+                for (int i = 0; i < myArray2.Length; i++)
+                    if (myArray2[i] % 2 == 0)
+                    {
+                        Count = Count + 1;
+                    }
+                return Count;
+            }
+
+            int[] myArray = new int[5];
+            myArray = GenerateRandomArray(5, 100, 999);
+            ShowArray(myArray);
+            Console.WriteLine("количество четных чисел массива " + CountOfEven(myArray));
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+            Console.WriteLine("Задача 36");
+            Console.WriteLine();
+            //Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+            //Найдите сумму элементов, стоящих на нечётных позициях.
+       
+            Console.Write($"Введи количество элементов массива: ");
+            int numberElements = Convert.ToInt32(Console.ReadLine());
+
+            int RandomNumbers(int numberElements, int min, int max)
+            {
+                int[] randomNumbers = new int[numberElements];
+                int sumElements = 0;
+                Console.Write("Получившийся массив: ");
+
+                for (int i = 0; i < randomNumbers.Length; i++)
+                {
+                    randomNumbers[i] = new Random().Next(min, max);
+
+                    Console.Write(randomNumbers[i] + " ");
+
+                    if (i % 2 != 1)
+                    {
+                        sumElements = sumElements + randomNumbers[i];
+                    }
+                }
+                return sumElements;
+            }
+
+            int randomNumbers = RandomNumbers(numberElements, 1, 30);
+
+            Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
+
+          
+            Console.WriteLine();
+            Console.WriteLine("Задача 38");
+            Console.WriteLine();
+            //Задайте массив вещественных чисел. Найдите разницу между максимальным
+            // и минимальным элементов массива.
+           
+            int Find(int[] array)
+                {
+                    int result = 0;
+                    int min = 0;
+                    int max = 0;
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (array[min] > array[i])
+                            min = i;
+                        else if (array[max] < array[i])
+                            max = i;
+                    }
+                    Console.WriteLine("min = " + array[min] + "\tmax = " + array[max]);
+                    if (array[min] > array[max])
+                        result = array[min] - array[max];
+                    else if (array[max] > array[min])
+                        result = array[max] - array[min];
+                    return result;
+                }
             
-            //FillArray(plusNumbers);
-            //PrintArray(plusNumbers);
+            Console.Write("введите размер массива: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+            Console.Write("введите минимальное значение: ");
+            int Min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите максимальное значение: ");
+            int Max = Convert.ToInt32(Console.ReadLine());
+            int temp = 0;
+            if (Min > Max)
+            {
+                temp = Max;
+                Max = Min;
+                Min = temp;
+            }
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(Min, Max);
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(array[i] + "\t");
 
-
-
-
+          
+            Console.WriteLine("разница между минимальным и максимальным элементом массива = " + Find(array));
+            
         }
     }
 
 }
+
+
+
+
+
 
 
 
