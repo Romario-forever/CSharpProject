5b25f02c6bf325c2c8a70ff57693965c4d49361e
Roman <yashenkovrv@gmail.com>
2022-10-04 14:24:05 +0300

    add HomeWork005

---
+++ HomeWork005/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Задача 34");
+            Console.WriteLine();
+            //Задайте массив заполненный случайными положительными трёхзначными числами. 
+            //Напишите программу, которая покажет количество чётных чисел в массиве.
+            //[345, 897, 568, 234] -> 2
+            
+            int[] plusNumbers = new int[4];
+            
+            int[] numbers = plusNumbers;
+            int length = numbers.Length;
+            int index = 0;
+                
+                while (index < length)
+                {
+                    numbers[index] = new Random().Next(99, 1000);
+                    index++;
+                    Console.WriteLine(numbers(index));
+                }
+            
+            
+            /*void PrintArray(int[] values)
+            {
+                int count = values.Length;
+                int position = 0;
+                while (position < count)
+                {
+                    Console.WriteLine(values(position));
+                    position++;
+                }
+            }*/
+            
+            //FillArray(plusNumbers);
+            //PrintArray(plusNumbers);
+
+
+
+
+        }
+    }
+
+}
+
+
+
+
+
