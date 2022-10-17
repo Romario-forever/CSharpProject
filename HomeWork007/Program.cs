09b3af4c5ab374421a811e66060de08f6c6b86d8
Roman <yashenkovrv@gmail.com>
2022-10-17 15:00:31 +0300

    Фиксация изменений

--- HomeWork007/Program.cs
+++ HomeWork007/Program.cs
@@ -107,21 +107,81 @@ void Zadacha52()
         Console.WriteLine();
    
         for (int j = 0; j < solid.GetLength(1); j++)
+        
         {   
             double average = 0;
-                           
+               
             for (int i = 0; i < solid.GetLength(0); i++)
             {
                 average += solid[i, j];
                           
             }
             Console.Write($"{average / solid.GetLength(0)}; ");  
-                       
+                  
         }
-
     }
+        
     FillArray(solid);
 
 }
 Zadacha52();
 
+
+// Console.WriteLine();
+// Console.WriteLine("Задача 62\n");
+// //По желанию
+// // Напишите программу, которая заполнит спирально массив 4 на 4.
+// // Например, на выходе получается вот такой массив:
+// //  1  2  3  4
+// // 12 13 14  5
+// // 11 16 15  6
+// // 10  9  8  7
+// void Zadacha62()
+// {
+//     int[,] spiralArray = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+//     CreateSpiralArray(spiralArray);
+
+//     void CreateSpiralArray(int[,] spiralArray)
+//     {
+//         for (int i = 0; i < spiralArray.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < spiralArray.GetLength(1); j++)
+//             {
+//                 if (spiralArray[i, j] / 10 <= 0)
+//                     Console.Write($" {spiralArray[i, j]} ");
+
+//                 else Console.Write($"{spiralArray[i, j]} ");
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+//     int temp = 1;
+//     int i = 0;
+//     int j = 0;
+//     int Ibeg = 0, Ifin = 0, Jbeg = 0, Jfin = 0;
+
+//     while (temp <= spiralArray.GetLength(0) * spiralArray.GetLength(1))
+//     {
+//         spiralArray[i, j] = temp;
+//         temp++;
+        
+//         if (i == Ibeg && j < spiralArray.GetLength(1) - Jfin - 1)
+//             j++;
+//         else if (i == spiralArray.GetLength(1) - 1 && i < spiralArray.GetLength(0) - Ifin - 1)
+//             i++;
+//         else if (i == spiralArray.GetLength(0) - Jfin -1 && j > Jbeg)
+//             j--;
+//         else
+//             i--;
+        
+//         if ((i == Ibeg + 1) && (j == Jbeg) && (Jbeg != spiralArray.GetLength(1) - Jfin - 1))
+//         {
+//             ++Ibeg;
+//             ++Ifin;
+//             ++Jbeg;
+//             ++Jfin;
+//         }
+//     }
+
+// }
+// Zadacha62();
--- HomeWork008/Program.cs
+++ HomeWork008/Program.cs
@@ -1,2 +1,156 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine();
+Console.WriteLine("Задача 56\n");
+//Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+//Программа считает сумму элементов в каждой строке 
+//и выдаёт номер строки с наименьшей суммой элементов.
+void Zadacha56()
+{
+    Random random = new Random();
+    int[,] rectangArray = new int[4, 5];
+    FillArray(rectangArray);
+    NumMinSumElementsLine(rectangArray);
+
+    void FillArray(int[,] rectangArray)
+    {
+        for (int i = 0; i < rectangArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < rectangArray.GetLength(1); j++)
+            {
+                rectangArray[i, j] = new Random().Next(10, 30);
+                Console.Write($"{rectangArray[i, j]}  ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    void NumMinSumElementsLine(int[,] rectangArray)
+    {
+        int minLine = 0;
+        int minSumLine = 0;
+        int sumLine = 0;
+        for (int i = 0; i < rectangArray.GetLength(1); i++)
+        {
+            minLine += rectangArray[0, i];
+        }
+        for (int i = 0; i < rectangArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < rectangArray.GetLength(1); j++)
+                sumLine += rectangArray[i, j];
+            if (sumLine < minLine)
+            {
+                minLine = sumLine;
+                minSumLine = i;
+            }
+            Console.WriteLine(sumLine);
+            sumLine = 0;
+        }
+        Console.Write($"{minSumLine + 1} строка");
+    }
+    Console.WriteLine();
+
+}
+Zadacha56();
+
+Console.WriteLine();
+Console.WriteLine("Задача 58\n");
+//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+void Zadacha58()
+{
+    Random random = new Random();
+    int[,] firstMatrix = new int[3, 3];
+    int[,] secondMatrix = new int[3, 3];
+    int[,] resultMatrix = new int[3, 3];
+    CreateArray1(firstMatrix);
+    CreateArray2(secondMatrix);
+    MultiplyMatrix(firstMatrix, secondMatrix, resultMatrix);
+
+
+    void CreateArray1(int[,] firstMatrix)
+    {
+        Console.WriteLine($"Первая матрица: ");
+        for (int i = 0; i < firstMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < firstMatrix.GetLength(1); j++)
+            {
+                firstMatrix[i, j] = new Random().Next(1, 10);
+                Console.Write($"{firstMatrix[i, j]}  ");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+
+    void CreateArray2(int[,] secondMatrix)
+    {
+        Console.WriteLine($"Вторая матрица: ");
+        for (int i = 0; i < secondMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < secondMatrix.GetLength(1); j++)
+            {
+                secondMatrix[i, j] = new Random().Next(1, 9);
+                Console.Write($"{secondMatrix[i, j]}  ");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+
+    void MultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix, int[,] resultMatrix)
+    {
+        Console.WriteLine($"Произведение первой и второй матриц:");
+        for (int i = 0; i < firstMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < secondMatrix.GetLength(1); j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < firstMatrix.GetLength(0); k++)
+                {
+                    sum += firstMatrix[i, k] * secondMatrix[k, j];
+                }
+                resultMatrix[i, j] = sum;
+                Console.Write($"{resultMatrix[i, j]}  ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+Zadacha58();
+
+Console.WriteLine();
+Console.WriteLine("Задача 60\n");
+//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+void Zadacha60()
+{
+    int[,,] cube = new int[2, 2, 2];
+    CreateCube(cube);
+
+
+    void CreateCube(int[,,] cube)
+    {
+        Console.WriteLine($"Трехмерный массив : ");
+        for (int i = 0; i < cube.GetLength(0); i++)
+        {
+            for (int j = 0; j < cube.GetLength(1); j++)
+
+            {
+                for (int k = 0; k < cube.GetLength(2); k++)
+                {
+                    cube[i, j, k] = new Random().Next(10, 100);
+                    Console.Write($"{cube[i, j, k]} ({i},{j},{k}) ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+
+}
+Zadacha60();
+
